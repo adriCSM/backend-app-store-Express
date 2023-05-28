dc6361bd4b6fbe18a3b98b0829d40e629bb5a2f3
Adri Candra Saputra Mangidi <adricandrasaputramangidi@gmail.com>
2023-05-29 00:21:13 +0800

    update secure login

--- controller/auth.js
+++ controller/auth.js
@@ -59,7 +59,6 @@ module.exports = class {
                         },
                     );
                     res.cookie('REFRESH_TOKEN', refreshToken, {
-                        httpOnly: true,
                         maxAge: 24 * 60 * 60 * 1000,
                         secure: true,
                     });
