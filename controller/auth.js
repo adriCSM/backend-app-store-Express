9866b065a8fccd163fcb19b338d8a751fc0acd7c
Adri Candra Saputra Mangidi <adricandrasaputramangidi@gmail.com>
2023-05-31 01:46:02 +0800

    api chat whatsApp

--- controller/auth.js
+++ controller/auth.js
@@ -6,6 +6,7 @@ const { Client, LocalAuth } = require('whatsapp-web.js');
 const qrcode = require('qrcode-terminal');
 const client = new Client({
     authStrategy: new LocalAuth(),
+    executablePath: '/path/to/chromium-binary',
 });
 client.on('qr', (qr) => {
     qrcode.generate(qr, { small: true });
