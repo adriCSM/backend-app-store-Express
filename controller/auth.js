b3401ac6f1e62bdffc0afbdb75d82923c19bbc1c
adriCSM <109715820+adriCSM@users.noreply.github.com>
2023-05-30 18:05:52 +0800

    Update auth.js

--- controller/auth.js
+++ controller/auth.js
@@ -86,6 +86,7 @@ module.exports = class {
                         maxAge: 24 * 60 * 60 * 1000,
                         secure: true,
                         domain: 'https://adricsm.github.io/store/',
+                         sameSite: 'none',
                     });
                     res.status(200).json({ accessToken });
                 } catch (err) {
