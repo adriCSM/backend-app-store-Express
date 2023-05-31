470f465c7ada57c58b6ad6699d0a857f1add3b19
Adri Candra Saputra Mangidi <adricandrasaputramangidi@gmail.com>
2023-05-31 19:03:37 +0800

    update admin wa

--- index.js
+++ index.js
@@ -6,15 +6,10 @@ const cors = require('cors');
 const cookieParser = require('cookie-parser');
 const app = express();
 
+app.use(cors({ credentials: true, origin: ['http://localhost:8080', 'https://adricsm.github.io', 'http://127.0.0.1:5500'] }));
 app.use(cookieParser());
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(
-    cors({
-        origin: ['http://localhost:8080', 'https://adricsm.github.io', 'http://127.0.0.1:5500'],
-        credentials: true,
-    }),
-);
 mongoose
     .connect(process.env.URI)
     .then(() => {
