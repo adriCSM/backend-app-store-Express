c9d23afa7e915c295c375e497cdce0c5e908c624
Adri Candra Saputra Mangidi <adricandrasaputramangidi@gmail.com>
2023-05-31 19:23:10 +0800

    update admin wa

--- middleware/verifyAccesToken.js
+++ middleware/verifyAccesToken.js
@@ -3,10 +3,10 @@ const verifyAccessToken = (req, res, next) => {
     const authorization = req.headers.authorization;
     const token = authorization && authorization.split(' ')[1];
     if (!token) {
-        res.sendStatus(401);
+        res.status(401).json({ message: 'Tidak terdapat token' });
     } else {
         jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
-            if (err) res.sendStatus(403);
+            if (err) res.status(403).json({ message: 'Verifikasi token failed' });
             else {
                 req.profile = decoded;
                 next();
