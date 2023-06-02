45437182158c9cc2b19f2fcc9c77e62cbdc19c84
Adri Candra Saputra Mangidi <adricandrasaputramangidi@gmail.com>
2023-06-02 14:17:55 +0800

    initial commit

--- controller/auth.js
+++ controller/auth.js
@@ -90,7 +90,7 @@ module.exports = class {
                         httpOnly: true,
                         expires: new Date(Date.now() + 8 * 3600000),
                         secure: true,
-                        sameSite: None,
+                        sameSite: false,
                     });
                     res.status(200).json({ accessToken });
                 } catch (err) {
