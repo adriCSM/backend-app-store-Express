1089ec6352fbc01a3bfbbb24097728c6fed4f18f
Adri Candra Saputra Mangidi <adricandrasaputramangidi@gmail.com>
2023-06-02 11:00:56 +0800

    epdate executabelPath

--- index.js
+++ index.js
@@ -6,7 +6,12 @@ const cors = require('cors');
 const cookieParser = require('cookie-parser');
 const app = express();
 
-
+app.use(function (req, res, next) {
+    res.header('Access-Control-Allow-Origin', 'https://adricsm.github.io'); // Ubah URL ini sesuai dengan alamat frontend Anda
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    res.header('Access-Control-Allow-Credentials', true); // Izinkan pengiriman cookie
+    next();
+});
 app.use(cookieParser());
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: false }));
