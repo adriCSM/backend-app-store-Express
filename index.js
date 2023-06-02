588b422a7435f8cd94a045226bac440896e82bf7
Adri Candra Saputra Mangidi <adricandrasaputramangidi@gmail.com>
2023-06-02 14:54:10 +0800

    update credentials

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const cors = require('cors');
 const cookieParser = require('cookie-parser');
 const app = express();
 
-app.use(cors({ credentials: true, origin: ['http://localhost:8080', 'https://adricsm.github.io'] }));
+app.use(cors({ origin: ['http://localhost:8080', 'https://adricsm.github.io'] }));
 
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: false }));
